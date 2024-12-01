97ee9064418a975a68d9cf14e39428fc4408c5a5
Rumen Terziev <106109989+RumenTerziev@users.noreply.github.com>
2024-12-01 12:46:09 +0200

    Initialized project

---
+++ src/main/java/com/example/spring_grpc_data_streamer/SpringGrpcDataStreamerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_grpc_data_streamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcDataStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrpcDataStreamerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/spring_grpc_data_streamer/service/DataStreamService.java
@@ -0,0 +1,45 @@
+package com.example.spring_grpc_data_streamer.service;
+
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+import src.main.proto.DataStreamerGrpc;
+import src.main.proto.StreamRequest;
+import src.main.proto.StreamResponse;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Slf4j
+@GrpcService
+public class DataStreamService extends DataStreamerGrpc.DataStreamerImplBase {
+
+    @Override
+    public void streamData(StreamRequest request, StreamObserver<StreamResponse> responseObserver) {
+        Duration duration = Duration.ofSeconds(10);
+        Instant startTime = Instant.now();
+
+        try {
+            while (true) {
+                Duration elapsedTime = Duration.between(startTime, Instant.now());
+                if (elapsedTime.compareTo(duration) > 0) {
+                    break;
+                }
+
+                String message = String.format("Elapsed time: %.2f seconds", elapsedTime.toMillis() / 1000.0);
+                log.info(message);
+
+                StreamResponse response = StreamResponse.newBuilder()
+                        .setMessage(message)
+                        .build();
+                responseObserver.onNext(response);
+
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            responseObserver.onCompleted();
+        }
+    }
+}
